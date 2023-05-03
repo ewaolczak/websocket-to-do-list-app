02798891575ed252233f075a28fd97e36ebfb3e1
Ewa Olczak <ewa.olczak@gmail.com>
2023-05-04 00:20:15 +0200

    Add websockets, but with error

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,51 @@
 import io from 'socket.io-client';
+import { useEffect, useState } from 'react';
+import shortid from 'shortid';
 
 const App = () => {
+  const [socket, setSocket] = useState(null);
+  const [tasks, setTasks] = useState([]);
+  const [taskName, setTaskName] = useState('');
+
+  useEffect(() => {
+    const socket = io('http://localhost:8000');
+    setSocket(socket);
+    socket.on('updateData', (tasks) => {
+      updateTasks(tasks);
+    });
+
+    socket.on('addTask', (task) => {
+      addTask(task);
+    });
+
+    socket.on('removeTask', (id) => {
+      removeTask(id);
+    });
+  }, []);
+
+  const updateTasks = (tasksData) => {
+    setTasks(tasksData);
+  };
+
+  const addTask = (task) => {
+    setTasks((tasks) => [...tasks, task]);
+    setTaskName('');
+  };
+
+  const removeTask = (taskId, isLocal) => {
+    setTasks((tasks) => tasks.filter((task) => task.id !== taskId));
+    if (isLocal) {
+      socket.emit('removeTask', taskId);
+    }
+  };
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    const task = { id: shortid.generate(), name: taskName };
+    addTask(task);
+    socket.on('addTask', task);
+  };
+
   return (
     <div className='App'>
       <header>
@@ -11,21 +56,27 @@ const App = () => {
         <h2>Tasks</h2>
 
         <ul className='tasks-section__list' id='tasks-list'>
-          <li className='task'>
-            Shopping <button className='btn btn--red'>Remove</button>
-          </li>
-          <li className='task'>
-            Go out with a dog <button className='btn btn--red'>Remove</button>
-          </li>
+          {tasks.map((task) => (
+            <li key={task.id} className='task'>
+              <span>{task.name}</span>
+              <button
+                className='btn btn--red'
+                onClick={() => removeTask(task.id, true)}>
+                Remove
+              </button>
+            </li>
+          ))}
         </ul>
 
-        <form id='add-task-form'>
+        <form id='add-task-form' onSubmit={(e) => submitForm(e)}>
           <input
             className='text-input'
             autocomplete='off'
             type='text'
             placeholder='Type your description'
             id='task-name'
+            value={taskName}
+            onChange={(e) => setTaskName(e.target.value)}
           />
           <button className='btn' type='submit'>
             Add
--- server.js
+++ server.js
@@ -1,23 +1,22 @@
 const express = require('express');
+const app = express();
 const socket = require('socket.io');
 
-const app = express();
+const tasks = []; // { id: 1, name: "Shopping" }
+
 const server = app.listen(8000, () => {
   console.log('Server is running on port: 8000');
 });
 const io = socket(server);
 
-const tasks = []; // { id: 1, name: "Shopping" }
-
 io.on('connection', (socket) => {
-  console.log('New client! Its id - ' + socket.id);
+  console.log('Client connected with ID:', socket.id);
   socket.emit('updateData', tasks);
-
   socket.on('addTask', (task) => {
+    console.log('Task added');
     tasks.push(task);
     socket.broadcast.emit('addTask', task);
   });
-
   socket.on('removeTask', (task) => {
     tasks.splice(tasks.indexOf(task), 1);
     socket.broadcast.emit('removeTask', task);
@@ -25,5 +24,5 @@ io.on('connection', (socket) => {
 });
 
 app.use((req, res) => {
-  res.status(404).send('404 not found...');
+  res.status(404).send('404: Page not found...');
 });
