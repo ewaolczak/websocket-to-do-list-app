b863efb74290d267e8ccbeb52b83d693dfd868a4
Ewa Olczak <ewa.olczak@gmail.com>
2023-05-03 11:04:09 +0200

    In progress adding listeners

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
 const express = require('express');
+const socket = require('socket.io');
 
 const app = express();
 
 const server = app.listen(8000, () => {
   console.log('Server is running on port: 8000');
+});
+const io = socet(server);
+
+const tasks = []; // { id: 1, name: "Shopping" }
+
+io.on('connection', (socket) => {
+  console.log('New client! Its id - ' + socket.id);
+  socket.emit('updateData', tasks);
+
+  socket.on('addTask', task) => {
+    tasks.push({ id: socket.id, task: task})
+  }
+});
+
+
+
+app.use((req, res) => {
+  res.status(404).send('404 not found...');
 });
